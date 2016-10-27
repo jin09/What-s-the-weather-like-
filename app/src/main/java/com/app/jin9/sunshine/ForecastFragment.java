8d3cff55502a87299e68dbb3faa9ed6513085467
jin09 <supergautam995@gmail.com>
2016-10-27 22:51:17 +0530

    added refresh menu to the ForecastFragment

--- app/src/main/java/com/app/jin9/sunshine/ForecastFragment.java
+++ app/src/main/java/com/app/jin9/sunshine/ForecastFragment.java
@@ -6,13 +6,18 @@ package com.app.jin9.sunshine;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -28,9 +33,36 @@ import java.util.List;
  */
 public class ForecastFragment extends Fragment {
     private ArrayAdapter<String> mForecastAdapter;
+    private static final String TAG = ForecastFragment.class.getSimpleName();
 
     public ForecastFragment() {
 
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //set options menu to true
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if(id == R.id.action_refresh){
+            Toast.makeText(getContext(),"refresh menu HIT", Toast.LENGTH_SHORT).show();
+            Log.d(TAG,"refresh menu is HIT !!!!");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
