ceb9955069a2ce99dbb183ab90ce31436885ef6e
jin09 <supergautam995@gmail.com>
2016-10-28 16:22:05 +0530

    settings menu linked to a newly created settings activity

--- app/src/main/java/com/app/jin9/sunshine/MainActivity.java
+++ app/src/main/java/com/app/jin9/sunshine/MainActivity.java
@@ -1,5 +1,6 @@
 package com.app.jin9.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -33,8 +34,8 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if(id == R.id.action_settings){
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
